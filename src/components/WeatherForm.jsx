9322a26c33795ae6645f9250a7a050b3ce60b64b
IzzyCode <israel@izzycode.co>
2018-10-02 21:10:37 -0400

    Finish Components I

---
+++ src/components/WeatherForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/weather-form.css'
+
+class WeatherForm extends React.Component {
+  render(){
+    return (
+      <form className="weather-form">
+        <h1 className="form-title">Find your local Weather!</h1>
+        <div>
+          <label htmlFor="city">Enter city, state</label>
+          <input type="text" name="city" placeholder="ex.: Los Angeles, CA" />
+        </div>
+        <footer>
+          <input type="submit" value="Find my weather!" />
+        </footer>
+      </form>
+    )
+  }
+}
+
+export default WeatherForm;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/global.css';
+import WeatherApp from './components/WeatherApp'
+
+
+render( <WeatherApp />, document.querySelector("#app") );
